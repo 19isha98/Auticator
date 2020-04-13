73f9410b381ffe3d28adc344f113d9de492a7d5a
19isha98 <kansalisha1998@gmail.com>
2020-04-14 03:54:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <div>
+    <div className="header">
+     <h1>autiCator
+      <span className="options">
+      <button className="buttons" variant="outline-primary">
+  	Play Games   
+  	</button >{' '}
+    <button className="buttons" variant="outline-primary">Primary</button>{' '}
+  	<button className="buttons" variant="outline-primary">
+  	Watch Cartoons
+  	</button>{' '}
+  	<button className="buttons" variant="outline-primary">
+  	Session Interactions
+  	</button>{' '}
+    </span>
+    </h1>  
+    </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  	<p><h2>Get ready to see some amazing videos and cartoons and some fun games to play!</h2></p>
+    <p>
+  	<h2>WHY US?</h2>  
+       <img align="right" src="Brain.png" alt="Brain" /><br/>
+      We are a platform which can provide a child with special needs what they need the most, interaction. An environment where they can learn and play as well as as interact with others.<br/>
+      The app is parent as well as child friendly. The content have been developed with the help of research and understanding.We provide a platform which can easily be accessed by child even without the help of the parent.
+      This app provides a calm and compose environment for the child so that they can learn enthusiatically.<br/>
+      At home learning while having interactive sessions with the tutors.<br/>
+      </p>
+      <h2>Get ready to see some amazing videos and cartoons and some fun games to play!</h2>
+    <br/>
+    </div>
+   <div className="connect">
+     CONNECT WITH US THROUGH:
+     <img  align="right" src="connect.jpeg" alt="Brain" />
+ 	<ul>
+     	<li>Instagram{" "}
+     	<span role="img" aria-label="Emoji icon of a instagram">       	🚀
+     	</span>
+     	</li>
+     	<li>Facebook{" "}
+     	<span role="img" aria-label="Emoji icon of a rocket">
+       	🚀
+     	</span>
+     	</li> 
+     	<li>Twitter{" "}
+     	<span role="img" aria-label="Emoji icon of a rocket">
+       	🚀
+     	</span>
+     	</li>
+ 	</ul >
+   </div>
+   <p>
+       
+       
+    </p>
     </div>
   );
 }
